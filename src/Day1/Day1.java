32bf7fc732939551e612baabefdbb64a62350bcf
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-02 03:10:52 +0100

    Day1 completed!

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -4,18 +4,26 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day1 {
-	final Integer startFrequency = 0;
+	Boolean didOutputPart1 = false;
+	Boolean partTwoDone = false;
+	Integer counter = 0;
+	List<Integer> frequencies = new ArrayList<>();
 
 	public static void main(String[] args) {
 		new Day1().go();
 	}
 
 	void go() {
+		while (!partTwoDone)
+			readfile();
+	}
+
+	void readfile() {
 		Charset cs = Charset.forName("UTF-8");
-		Integer counter = startFrequency;
 		Path puzzleInput = Paths.get("src/Day1/input");
 
 		try {
@@ -31,12 +39,24 @@ public class Day1 {
 				} else {
 					throw new Exception("Unexpected puzzle data");
 				}
+
+				if (!partTwoDone) {
+					if (frequencies.contains(counter)) {
+						System.out.println("Part 2: " + counter);
+						partTwoDone = true;
+					} else {
+						frequencies.add(counter);
+					}
+				}
 			}
 		} catch (Exception ex) {
 			System.err.println("Error: " + ex.getMessage());
 			System.exit(1);
 		}
 
-		System.out.printf("Resulting frequency: %d\n", counter.intValue());
+		if (!didOutputPart1) {
+			System.out.printf("Resulting frequency: %d\n", counter.intValue());
+			didOutputPart1 = true;
+		}
 	}
 }
