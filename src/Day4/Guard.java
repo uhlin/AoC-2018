c606159147bcca566955d048f2883ca048d187a1
Markus Uhlin <markus.uhlin@bredband.net>
2018-12-12 06:02:16 +0100

    Update class

--- src/Day4/Guard.java
+++ src/Day4/Guard.java
@@ -1,12 +1,71 @@
 package Day4;
 
 class Guard {
+	private static final int ASLEEP_ARRAY_SIZE = 59;
+
 	private Integer id;
 	private Integer minutesAsleep;
+	private int[] asleep;
 
 	Guard(Integer id) {
 		this.id = id;
 		this.minutesAsleep = 0;
+		this.asleep = new int[ASLEEP_ARRAY_SIZE];
+		for (int i = 0; i < ASLEEP_ARRAY_SIZE; i++)
+			(asleep[i]) = 0;
+	}
+
+	public int getHits() {
+		int hits = 0;
+
+		for (int i = 0; i < ASLEEP_ARRAY_SIZE; i++) {
+			if (asleep[i] > hits)
+				hits = asleep[i];
+		}
+
+		return hits;
+	}
+
+	public int minuteMostFrequentlyAsleep() {
+		int hits = 0;
+		int i;
+
+		for (i = 0; i < ASLEEP_ARRAY_SIZE; i++) {
+			if (asleep[i] > hits)
+				hits = asleep[i];
+		}
+
+		for (i = 0; i < ASLEEP_ARRAY_SIZE; i++) {
+			if (asleep[i] == hits)
+				break;
+		}
+
+		return i;
+	}
+
+	/* ---------------------------------------- */
+
+	public void increaseMinuteHitsFor(final int whichMinute) {
+		for (int i = 0; i < ASLEEP_ARRAY_SIZE; i++) {
+			if (i == whichMinute) {
+				(asleep[i]) ++;
+				return;
+			}
+		}
+
+		System.err.println("increaseMinuteHitsFor() error");
+		System.exit(1);
+	}
+	public void decreaseMinuteHitsFor(final int whichMinute) {
+		for (int i = 0; i < ASLEEP_ARRAY_SIZE; i++) {
+			if (i == whichMinute) {
+				(asleep[i]) --;
+				return;
+			}
+		}
+
+		System.err.println("decreaseMinuteHitsFor() error");
+		System.exit(1);
 	}
 
 
